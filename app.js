3de3dc2db9142be62fe5569cb1afd1b59c091d83
jacazhang <1228465122@qq.com>
2019-12-03 21:17:28 +0800

    work14

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ const  openDefaultBrowser = function (url) {
     var exec = require('child_process').exec
     switch (process.platform) {
         case"darwin":
-            exec('open' +url)
+            exec('open ' +url)
             break
         case"win32":
-            exec('start' +url)
+            exec('start ' +url)
             break
         default:
             exec('xdg-open',[url])
